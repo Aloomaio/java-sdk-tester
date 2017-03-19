e9f052de93f90ed5d6eaefd141899c6f96822c54
Yonatan Kiron <yonatankiron@gmail.com>
2017-03-19 15:37:01 +0200

    tested

--- src/main/java/com/alooma/javatester/App.java
+++ src/main/java/com/alooma/javatester/App.java
@@ -1,49 +1,49 @@
 package com.alooma.javatester;
 
+import com.google.devtools.common.options.OptionsParser;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
 import org.apache.logging.log4j.core.appender.SocketAppender;
-import org.apache.logging.log4j.core.layout.JsonLayout;
-import org.apache.logging.log4j.core.net.ssl.SslConfiguration;
-import org.apache.logging.log4j.core.net.ssl.StoreConfigurationException;
-import org.apache.logging.log4j.core.net.ssl.TrustStoreConfiguration;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+
+import java.util.Collections;
+
 import static org.apache.logging.log4j.LogManager.getLogger;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	private static final Logger reporter = getLogger("alooma-reporter");
 
-    public static void main( String[] args ) {
-		if (args.length != 2) {
-			System.out.println("Usage: java -jar javasdk-tester.jar <alooma hostname> <num of messages>");
-			return;
-		}
+	public static void main( String[] args ) {
+        OptionsParser parser = OptionsParser.newOptionsParser(TesterParser.class);
+        parser.parseAndExitUponError(args);
+        TesterParser options = parser.getOptions(TesterParser.class);
+        if (options.numOfEvents == 0 || options.host.isEmpty() || options.token.isEmpty()){
+            printUsage(parser);
+            return;
+        }
 
-		int msgNum = Integer.parseInt(args[1]);
-
-		Layout<String> jsonLayout = JsonLayout.createLayout(false, false, false, true, true, null);
+		Layout<String> patternLayout = PatternLayout.createDefaultLayout();
+		ConsoleAppender.createDefaultAppenderForLayout(patternLayout);
 		SocketAppender socketAppender;
 		socketAppender = SocketAppender.createAppender(
-				args[0], // alooma hostname
-				"5001",
-				"SSL", // protocol = "SSL"
-				null,
-				0,
-				null,
-				null,
+				options.host, // alooma hostname
+				options.port,
+				options.protocol, // protocol = "SSL"
+				null, 0, null, null,
 				"Alooma", // Logger name
 				"true", // immediateFlush = true
-				null,
-				jsonLayout,
-				null, null, null);
+				null, patternLayout, null, null, null);
 		socketAppender.start();
 		((org.apache.logging.log4j.core.Logger)reporter).addAppender(socketAppender);
-		for (int i = 0; i < msgNum; i++) {
-			reporter.info("{\"type\":\"java-sdk-tester\",\"message\":\"your message\",\"num\":{}}", i);
+		for (int i = 0; i < options.numOfEvents; i++) {
+			reporter.info("{\"token\":\"" + options.token + "\",\"message\":{\"stringField\":\"Java reporter - tester\",\"numField\":" + i +"}}");
 		}
-		System.out.println( "Hello World!" );
+	}
+
+    private static void printUsage(OptionsParser parser) {
+        System.out.println("Usage: java -jar javasdk-tester-new.jar OPTIONS");
+        System.out.println(parser.describeOptions(Collections.<String, String>emptyMap(),
+                OptionsParser.HelpVerbosity.LONG));
     }
 }
